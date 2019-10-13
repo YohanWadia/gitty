0f7a950141422fcf4e751a9f0973d2cab88a96ad
YoComp <persianpowerman@hotmail.com>
2019-10-13 18:18:00 +0300

    first gitty

--- src/First.java
+++ src/First.java
@@ -1,5 +1,7 @@
 public class First {
     public static void main(String[] args) {
         System.out.println("From our great Comp");
+
+        System.out.println("This one only for Win7");
     }
 }
