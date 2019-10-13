9e9290f84992975f56f309d709d7a6d53798dfe6
YoComp <persianpowerman@hotmail.com>
2019-10-13 18:19:31 +0300

    first gitty

--- src/First.java
+++ src/First.java
@@ -1,5 +1,8 @@
 public class First {
     public static void main(String[] args) {
+
         System.out.println("From our great Comp");
+
+        //first time to master added ... after win7 branch
     }
 }
