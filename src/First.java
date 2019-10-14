8e555b068694c6fd8b4da89dd29fbdcb8c309063
YoComp <persianpowerman@hotmail.com>
2019-10-14 10:36:15 +0300

    Special 1st entry

--- src/First.java
+++ src/First.java
@@ -2,6 +2,7 @@ public class First {
     public static void main(String[] args) {
         System.out.println("From our great Comp");
 
-        System.out.println("This one only for Win7");
+        System.out.println("Win7 Special Edition");
+        System.out.println("Win7 Special Edition");
     }
 }
